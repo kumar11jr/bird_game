4a09f74742882b78c7bd7a20a4896fa605f2dcf3
kumar11jr <kumar11jr@gmail.com>
2024-04-24 19:52:37 +0530

    set bg and bird

--- bird.java
+++ bird.java
@@ -14,13 +14,53 @@ public class bird extends JPanel {
     Image upperPipeImage;
     Image lowerPipeImage;
 
+
+
+    int birdX = frameWidth/8;
+    int birdY = frameHeight/2;
+    int birdWidth = 34;
+    int birdHeight = 24;
+
+
+
+    class Bird{
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img){
+            this.img = img;
+        }
+    }
+
+
+
+    Bird bird;
+
+
     bird(){
         setPreferredSize(new Dimension(frameWidth, frameHeight));
-        setBackground(Color.blue);
 
         backImage = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
         birdImage = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
         upperPipeImage = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
         lowerPipeImage = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+
+
+        bird = new Bird(birdImage);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+        // Your code here
+        g.drawImage(backImage, 0, 0, frameWidth, frameHeight, null);
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
     }
 }
