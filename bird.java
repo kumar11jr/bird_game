6fcd377490a8b438b27c67b5e49a60e14546f01b
kumar11jr <kumar11jr@gmail.com>
2024-04-25 00:24:01 +0530

    rm:non essentials

--- bird.java
+++ bird.java
@@ -174,10 +174,10 @@ public class bird extends JPanel implements ActionListener,KeyListener{
     }
 
     boolean collision(Bird a, pipe b) {
-        return a.x < b.x + b.width &&   //a's top left corner doesn't reach b's top right corner
-               a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
-               a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
-               a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
+        return a.x < b.x + b.width && 
+               a.x + a.width > b.x && 
+               a.y < b.y + b.height &&
+               a.y + a.height > b.y;
     }
 
 
